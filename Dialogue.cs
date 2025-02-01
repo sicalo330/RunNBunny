ae5afdc31e0cdbb05edbf010dc1a71f2e37a3a25
sicalo330 <noah.esteban9@gmail.com>
2025-02-01 16:50:31 -0500

    Avanzar con la lógica de los dialogos

--- Dialogue.cs
+++ Dialogue.cs
@@ -5,51 +5,82 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    public float textSpeed;
-    private int index;
+    // public TextMeshProUGUI textComponent;
+    // public string[] lines;
+    // public float textSpeed;
+    // private int index;
     // Start is called before the first frame update
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    private float typingTime = 0.05f;
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
     void Start()
     {
-        textComponent.text = string.Empty;
-        StartDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            if(textComponent.text == lines[index]){
-                NextLine();
+        if(isPlayerInRange && Input.GetButtonDown("Fire1")){
+            if(!didDialogueStart){
+                StartDialogue();
+            }
+            else if(dialogueText.text == dialogueLines[lineIndex]){
+                NextDialogue();
             }
             else{
                 StopAllCoroutines();
-                textComponent.text = lines[index];
+                dialogueText.text = dialogueLines[lineIndex];
             }
         }
     }
 
     void StartDialogue(){
-        index = 0;
-        StartCoroutine(TypeLine());
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine()); 
     }
 
-    IEnumerator TypeLine(){
-        foreach(char c in lines[index].ToCharArray()){
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    void NextLine(){
-        if(index < lines.Length - 1){
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
+    private void NextDialogue(){
+        lineIndex++;
+        //Si hay más palabras para decir, entonces que siga, sino entonces se desactiva el panel
+        if(lineIndex < dialogueLines.Length){
+            StartCoroutine(ShowLine());
         }
         else{
-            gameObject.SetActive(false);
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(true);//Es decir que se puede volver a hablar con el npc
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine(){
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[lineIndex]){
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")) {
+            isPlayerInRange = true;
+            dialogueMark.SetActive(true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player")) {
+            isPlayerInRange = false;
+            dialogueMark.SetActive(false);
         }
     }
 }
